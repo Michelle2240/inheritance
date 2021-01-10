c1e025e1981fdb72d4aad79a518c3dac2016acfa
Michelle2240 <selestinamichelle22@gmail.com>
2021-01-10 20:03:11 +0530

    added inheritance

--- sketch.js
+++ sketch.js
@@ -4,9 +4,15 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
+var backgroundImage;
+
+function preload()
+{
+  backgroundImage = loadImage("sprites/base.png");
+}
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+      createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
@@ -33,7 +39,7 @@ function setup(){
 }
 
 function draw(){
-    background(0);
+    background("white");
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
